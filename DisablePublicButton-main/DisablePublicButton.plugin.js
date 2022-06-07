068049afb0394f222f0f995de85c565f693b490e
rbxlscripts <104306541+rbxlscripts@users.noreply.github.com>
2022-06-07 18:20:42 +0200

    Add files via upload

---
+++ DisablePublicButton-main/DisablePublicButton.plugin.js
@@ -0,0 +1,38 @@
+/**
+ * @name DisablePublicButton
+ * @version 0.0.1
+ * @description Disable the public button on the sidebar.
+ * @author mstudio45#5590
+ * @authorId 586218413693730883
+ * @source https://github.com/rbxlscripts/DisablePublicButton
+ * @updateUrl https://raw.githubusercontent.com/rbxlscripts/DisablePublicButton/main/DisablePublicButton.plugin.js
+ */
+
+module.exports = class DisablePublicButton {
+    load() {
+        function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+
+        async function loop() {
+            await sleep(1000);
+            var publicButton = document.getElementById('bd-pub-li');
+            if (publicButton) {
+                publicButton.remove();
+                await sleep(1000);
+                if (publicButton) {
+                    publicButton.remove();
+                }
+                await sleep(1000);
+                try { publicButton.remove(); } catch { }
+            }
+            loop();
+        }
+        BdApi.showToast('Public button disabled.', { type: 'success' });
+        loop();
+    }
+    start() { 
+	    BdApi.showToast('Public button disabled.', { type: 'success' });
+    }
+    stop() { }
+};
---
+++ DisablePublicButton-main/README.md
@@ -0,0 +1,12 @@
+# Disable Public Button 
+
+[![Download][download-badge]][download-link]
+
+[download-badge]: https://img.shields.io/badge/Download-%233a71c1.svg?style=flat&logo=data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAgCAYAAAAIXrg4AAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAAd5gAAHeYBMKt3fAAAABl0RVh0U29mdHdhcmUAd3d3Lmlua3NjYXBlLm9yZ5vuPBoAAAE9SURBVEiJ7ZQ7TsNAFEWv06IAShOkEDD5UNCwB8RCWEYWAqkIYiEgZQERoqLgFz4hUIaC+tCM4cmyk4mxCyRfaSRL8+49781YI3kKOASmwBtw4OvzFnDJry58fZUlGGvme70IQCaVgBLwXwBADegDp0AjaxjQAAbACVCzG+fmGRgDYYJ5ZGpGCfuh80Ya/EwgqWpqQ0lDoLVE5y1JQ+eNVLUFXfdSWr0A7UUTAG1XazUFuvEudoFJrPAd2EsDuMZeY56PyJM0aqohDljU0Lzz7KSMbMMmJB9px/fStoFH/PWMuS9fyBbw4Bnu/cclQe7nhD8BO5nCDaQJ3CWEj/8cbiAbwI0JvwU2cwk3kDpw7FY91/BShSoAmpKOJK3knP0l6SwAriXt5xwe6SoAPiWtFgSYVST1JM2KCJfU+wadgFl0/0HGbQAAAABJRU5ErkJggg==
+[download-link]: https://downgit.github.io/#/home?url=https://github.com/rbxlscripts/DisablePublicButton/blob/main/DisablePublicButton.plugin.js
+
+`Remove the public button from the sidebar.`<br>
+**Without:**<br>
+![Without](https://mstudio45.has-no-bra.in/5yeztqHrv.png)<br>
+**With:**<br>
+![With](https://mstudio45.has-no-bra.in/5yezZ6OAJ.png)<br>
